f743b524482c67387ae0fe3e34621ff99e83f0e5
unknown <adeshgwork@gmail.com>
2026-04-01 23:52:33 +0530

    seo2

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -38,8 +38,8 @@ export default function App({ Component, pageProps }) {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
           <link rel="manifest" href="/site.webmanifest" />
-          {/* <link rel="preconnect" href="https://fonts.googleapis.com" /> */}
-          {/* <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" /> */}
+          <link href="https://fonts.googleapis.com" />
+          <link href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link
             href="https://fonts.googleapis.com/css2?family=Sora:wght@400;500;600;700&family=Space+Grotesk:wght@400;500;600;700&display=swap"
             rel="stylesheet"
